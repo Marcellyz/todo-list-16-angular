3059c22317fea8d8b8171e4661ba1bfd2bc20b28
Marcellyz <marcelly.eduarda4545@gmail.com>
2024-03-23 11:59:05 -0300

    commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,6 +22,9 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('todo-list-16 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('todo-list-16 app is running!')
+
+
+    ;
   });
 });
